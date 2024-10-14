774fbb9c6442d5a18b3127e9e7aefc905d201fdd
Dorian <98755570+Doori4N@users.noreply.github.com>
2024-10-14 14:01:59 +0200

    ✨ Init data-service

---
+++ backend/data-service/src/test/java/com/padd/data_service/DataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padd.data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
