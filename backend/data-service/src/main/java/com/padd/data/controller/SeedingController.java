e0e65093cc2d4b34a73a038ca13f4d03b2ddca38
Dorian <98755570+Doori4N@users.noreply.github.com>
2024-10-23 10:47:15 +0200

    Data service > Add seeding endpoint

---
+++ backend/data-service/src/main/java/com/padd/data/controller/SeedingController.java
@@ -0,0 +1,86 @@
+package com.padd.data.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.padd.data.entity.BloodPressure;
+import com.padd.data.entity.HealthRecord;
+import com.padd.data.entity.SleepPace;
+import com.padd.data.entity.User;
+import com.padd.data.repository.UserRepository;
+import com.padd.data.service.HealthDataService;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@RestController
+public class SeedingController {
+
+    private final HealthDataService healthDataService;
+    private final UserRepository userRepository;
+
+    public SeedingController(
+        HealthDataService healthDataService,
+        UserRepository userRepository
+    ) {
+        this.healthDataService = healthDataService;
+        this.userRepository = userRepository;
+    }
+    
+    @GetMapping("/seed/{userId}")
+    public ResponseEntity<String> saveHealthData(@PathVariable String userId) {
+        if (createFakeData(userId)) {
+            return new ResponseEntity<>("Fake data created for user : " + userId, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Failed to create fake data for user : " + userId, HttpStatus.NOT_FOUND);
+    }
+
+    private boolean createFakeData(String userId) {
+        Optional<User> user = userRepository.findById(Long.parseLong(userId));
+
+        if (!user.isPresent()) {
+            return false;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            HealthRecord healthRecord = new HealthRecord();
+            healthRecord.setUser(user.get());
+            
+            LocalDateTime timestamp = LocalDateTime.of(2024, 1, 1, 0, i, 0);
+            healthRecord.setTimestamp(timestamp);
+            
+            BloodPressure bloodPressure = new BloodPressure();
+            bloodPressure.setSystolic(120 + i);
+            bloodPressure.setDiastolic(80 + i);
+            healthRecord.setBloodPressure(bloodPressure);
+
+            healthRecord.setHeartRate(80 + i);
+            healthRecord.setStressLevel(2 + i);
+            healthRecord.setBloodOxygenation(98 + i);
+            healthRecord.setBodyTemperature(36.5 + i);
+            healthRecord.setAcceleration(10 + i);
+
+            healthDataService.createOrUpdateHealthRecord(healthRecord);
+        }
+
+        SleepPace sleepPace = new SleepPace();
+        sleepPace.setUser(user.get());
+
+        LocalDateTime timestamp = LocalDateTime.of(2024, 1, 1, 0, 1, 0);
+        sleepPace.setTimestamp(timestamp);
+
+        sleepPace.setSleepDuration(7);
+        sleepPace.setLightSlowSleep(4);
+        sleepPace.setDeepSlowSleep(1);
+        sleepPace.setDeepSlowParadoxSleep(1);
+        sleepPace.setParadoxSleep(1);
+
+        healthDataService.createOrUpdateSleepPace(sleepPace);
+
+        return true;
+    }
+
+}
